043255beb56ed8d9582109360e805bd22f81e306
Felix422 <marius.mf2004@gmail.com>
2019-08-24 01:01:29 +0200

    removed help and moved to info cog

--- cogs/tools.py
+++ cogs/tools.py
@@ -21,43 +21,6 @@ class Tools(commands.Cog):
     #     await ctx.send(embed=embed)
 
     @commands.command()
-    @commands.cooldown(rate=1, per=10.0, type=commands.BucketType.user)
-    async def help(self, ctx, command=None):
-        e = discord.Embed(colour=discord.Color.green())
-        if command is None:
-            e.add_field(name="Commands:",value=".showcase\n.noodleslap\n.wolfram\n.ping")
-            await ctx.send(embed=e)
-            return
-        elif command.lower() == "showcase" or command.lower() == "sc":
-            e.set_author(name="Showcase")
-            e.add_field(name="Usage:", value=".showcase [SC] [MSC(Optional)]\n.showcase clear")
-            e.add_field(name="Description:", value="Sets your SC tag", inline=False)
-            e.add_field(name="Aliases:", value="showcase, sc", inline=False)
-            await ctx.send(embed=e)
-            return
-        elif command.lower() == "noodleslap":
-            e.set_author(name="Noodlesllap")
-            e.add_field(name="Usage:", value=".noodleslap [user]")
-            e.add_field(name="Description:", value="Slaps someone with noddles", inline=False)
-            await ctx.send(embed=e)
-            return
-        elif command.lower() == "wolfram" or command.lower() == "w":
-            e.set_author(name="Wolfram")
-            e.add_field(name="Usage:", value=".wolfram [question]")
-            e.add_field(name="Description:", value="Queries Wolfram alpha", inline=False)
-            e.add_field(name="Aliases:", value="wolfram, w, we", inline=False)
-            await ctx.send(embed=e)
-            return
-        elif command.lower() == "ping":
-            e.set_author(name="Ping")
-            e.add_field(name="Usage:", value=".ping")
-            e.add_field(name="Description:", value="Shows the bots latency", inline=False)
-            await ctx.send(embed=e)
-            return
-        else:
-            await ctx.send(f"Command `{command}` not found")
-
-    @commands.command()
     @commands.has_role('Staff')
     async def setnick(self, ctx, member:discord.Member, *, nick):
         if member == ctx.guild.owner:
