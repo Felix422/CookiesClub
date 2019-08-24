0934d13f27187cc2f2d67b6b97d53422fe4d2452
Felix422 <marius.mf2004@gmail.com>
2019-08-24 21:32:32 +0200

    changed the regex a little to mess with potato

--- cogs/sc.py
+++ cogs/sc.py
@@ -26,7 +26,7 @@ class Showcase(commands.Cog):
                 await ctx.send("Please specify an SC item!")
             elif mysteryshowcase == 0:
                 if member.nick is not None:
-                    oldnick = re.search("^(.*?)(\(SC\s?#\s?[\d\+]*\)|)$", member.nick, re.IGNORECASE).group(1)
+                    oldnick = re.search("^(.*?)(\(SC\s?#\s?[-?\d\+]*\)|)$", member.nick, re.IGNORECASE).group(1)
                     fullnick = f"{oldnick}(SC#{showcase})"
                     if len(fullnick) > 32:
                         await ctx.send("Name is too long!")
@@ -44,7 +44,7 @@ class Showcase(commands.Cog):
                     return
             else:
                 if member.nick is not None:
-                    oldnick = re.search("^(.*?)(\(SC\s?#\s?[\d\+]*\)|)$", member.nick, re.IGNORECASE).group(1)
+                    oldnick = re.search("^(.*?)(\(SC\s?#\s?[-?\d\+]*\)|)$", member.nick, re.IGNORECASE).group(1)
                     fullnick = f"{oldnick}(SC#{showcase}+{mysteryshowcase})"
                     if len(fullnick) > 32:
                         await ctx.send("Nickname too long!")
@@ -63,7 +63,7 @@ class Showcase(commands.Cog):
         elif isinstance(showcase, str):
             if showcase == "clear":
                 if member.nick is not None:
-                    oldnick = re.search("^(.*?)(\(SC\s?#\s?[\d\+]*\)|)$", member.nick, re.IGNORECASE).group(1)
+                    oldnick = re.search("^(.*?)(\(SC\s?#\s?[-?\d\+]*\)|)$", member.nick, re.IGNORECASE).group(1)
                     await member.edit(nick=oldnick)
                     return
                 else:
