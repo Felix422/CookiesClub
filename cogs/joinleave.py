740e89589fff8528bbfbd8af58313d2ae258c417
Felix422 <marius.mf2004@gmail.com>
2019-08-25 16:57:56 +0200

    fixed messages for people joining on my test server getting sent to the main server

--- cogs/joinleave.py
+++ cogs/joinleave.py
@@ -9,24 +9,24 @@ class JoinLeave(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        print(f"{member} joined the server")
-        channelname = self.bot.get_channel(232705019349041152)
+        print(f"{member} joined {member.guild.name}")
+        channel = discord.utils.get(member.guild.text_channels, name="member_logs")
         embed = discord.Embed(description=f"{member.mention} {member.name}#{member.discriminator} ", colour=discord.Color.green(), timestamp=datetime.utcnow())
         embed.set_thumbnail(url=member.avatar_url)
         embed.add_field(name="Account age", value=f"Created {timeago.format(member.created_at, datetime.now())}")
         embed.set_footer(text=f"User ID:{member.id}")
         embed.set_author(name=f"{member.name} has joined the server", icon_url=member.avatar_url)
-        await channelname.send(embed=embed)
+        await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        print(f"{member} left the server")
-        channelname = self.bot.get_channel(232705019349041152)
+        print(f"{member} left {member.guild.name}")
+        channel = discord.utils.get(member.guild.text_channels, name="member_logs")
         embed = discord.Embed(description=f"{member.mention} {member.name}#{member.discriminator} ", colour=discord.Color.red(), timestamp=datetime.utcnow())
         embed.set_thumbnail(url=member.avatar_url)
         embed.set_footer(text=f"User ID:{member.id}")
         embed.set_author(name=f"{member.name} left the server", icon_url=member.avatar_url)
-        await channelname.send(embed=embed)
+        await channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(JoinLeave(bot))
