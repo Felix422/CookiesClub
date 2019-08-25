5d6d5fc0d4e0aeb67d6679988ab42b623409fbb6
Felix422 <marius.mf2004@gmail.com>
2019-08-26 01:18:49 +0200

    added basic moderation

---
+++ cogs/moderation.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+
+class Moderation(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["purge"])
+    @commands.has_role("Staff")
+    async def clear(self, ctx, amount : int):
+        await ctx.channel.purge(limit=amount + 1)
+        await ctx.send(f"Purged {amount} messages", delete_after=5)
+
+    @clear.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+            await ctx.send("How many messages do you want to purge?")
+
+    @commands.command()
+    @commands.has_role("Staff")
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split("#")
+        for ban_entry in banned_users:
+            user = ban_entry.user
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f"Unbanned {user.mention}")
+                return
+
+    @unban.error
+    async def unban_error(self, ctx, error):
+        if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+            await ctx.send("Who do you want to unban?")
+
+    @commands.command()
+    @commands.has_role("Staff")
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f"Banned {member.mention}!")
+
+    @ban.error
+    async def ban_error(self, ctx, error):
+        if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+            await ctx.send("Who do you want to ban?")
+
+    @commands.command()
+    @commands.has_role("Staff")
+    async def kick(self, ctx, member:discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(f"Kicked {member.mention}")
+
+    @kick.error
+    async def Kicked_error(self, ctx, error):
+        if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+            await ctx.send("Who do you want to kick?")
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
