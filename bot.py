251cce94be876ef3d2b44c8e2ba0685a2b78d716
Felix422 <marius.mf2004@gmail.com>
2019-08-26 12:38:45 +0200

    optimized a little and better logging

--- bot.py
+++ bot.py
@@ -12,25 +12,26 @@ class Bot(commands.Bot):
             command_prefix=COMMAND_PREFIX
         )
 
-        self.remove_command('help')
+        self.remove_command("help")
 
     async def on_connect(self):
         print("Connected to discord")
 
     async def on_ready(self):
 
-        for filename in filter(lambda filename: filename.endswith('.py'), os.listdir('cogs')):
+        for filename in filter(lambda filename: filename.endswith(".py"), os.listdir("cogs")):
             cog_name = filename[:-3]
-            print('Loading {}'.format(cog_name))
-            self.load_extension('cogs.{}'.format(cog_name))
+            print(f"Loading {cog_name}")
+            self.load_extension(f"cogs.{cog_name}")
 
-        print('Initializing aiohttp')
+        print("Initializing aiohttp")
         self.aiohttp = aiohttp.ClientSession(
             loop=self.loop,
             timeout=aiohttp.ClientTimeout(total=5)
         )
 
         await self.change_presence(status=discord.Status.online, activity=discord.Activity(name="all those bad suggestions", type=discord.ActivityType.watching))
+        print("Changed presence")
         print(f"Bot Logged in as {self.user.name} and ready for duty!")
 
 Bot().run(BOT_TOKEN)
