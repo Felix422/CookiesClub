7b1cefb92b1f1969117e5cd011c46fc26560fa06
Felix422 <marius.mf2004@gmail.com>
2019-08-29 13:51:38 +0200

    fixed/expanded unban command and added leave command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import datetime
 from discord.ext import commands
 
 class Moderation(commands.Cog):
@@ -23,20 +24,28 @@ class Moderation(commands.Cog):
 
     @commands.command()
     @commands.has_role("Staff")
-    async def unban(self, ctx, *, member):
+    async def unban(self, ctx, *, member=None):
+        if member is None:
+            await ctx.send("Who do you want to unban?")
         banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split("#")
+        try:
+            member_name, member_discriminator = member.split("#")
+        except ValueError:
+            await ctx.send("invalid member")
+            return
         for ban_entry in banned_users:
             if (ban_entry.user.name, ban_entry.user.discriminator) == (member_name, member_discriminator):
-                await ctx.guild.unban(user)
+                await ctx.guild.unban(ban_entry.user)
                 await ctx.send(f"Unbanned {ban_entry.user.name}")
-                print(f"Unbanned {ban.entry.user.name}")
+                print(f"{ban_entry.user} got unbanned from {ctx.guild.name}")
+                channel = discord.utils.get(ctx.guild.text_channels, name="member_logs")
+                embed = discord.Embed(description=f"{ban_entry.user.name}#{ban_entry.user.discriminator} ", colour=discord.Color.green(), timestamp=datetime.utcnow())
+                embed.set_thumbnail(url=ban_entry.user.avatar_url)
+                embed.set_footer(text=f"User ID:{ban_entry.user.id}")
+                embed.set_author(name=f"{ban_entry.user.name} got unbanned from the server", icon_url=ban_entry.user.avatar_url)
+                await channel.send(embed=embed)
                 return
-
-    @unban.error
-    async def unban_error(self, ctx, error):
-        if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-            await ctx.send("Who do you want to unban?")
+        # await ctx.send("Member couldnt be found or isnt banned")
 
     @commands.command()
     @commands.has_role("Staff")
@@ -59,6 +68,11 @@ class Moderation(commands.Cog):
     async def Kicked_error(self, ctx, error):
         if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
             await ctx.send("Who do you want to kick?")
+
+    @commands.command()
+    @commands.has_role("Staff")
+    async def leave(self, ctx):
+        await ctx.guild.leave()
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
