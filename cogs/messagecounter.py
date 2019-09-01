fee4b39d6a0d5fee9772e178bb0d165fad56b27d
Felix422 <marius.mf2004@gmail.com>
2019-09-01 16:44:15 +0200

    idek what i changed

--- cogs/messagecounter.py
+++ cogs/messagecounter.py
@@ -1,22 +1,22 @@
-import discord, random
+import discord
+from random import randint as random
 from discord.ext import commands
-from config import *
+from config import COUNTER_MESSAGES
 
 class MessageCounter(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.messages_required = random(200, 400)
         self.message_counter = 0
-        self.messages_required = random.randint(200, 400)
-        print(f"Message requirement is {self.messages_required}")
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.channel.name == "general":
             self.message_counter += 1
             if self.message_counter == self.messages_required:
-                index = random.randint(0, len(dictionary) - 1)
-                await message.channel.send(dictionary[index].format(message.author.mention))
+                index = random.randint(0, len(COUNTER_MESSAGES) - 1)
+                await message.channel.send(COUNTER_MESSAGES[index].format(message.author.mention))
                 print(f"Sent counter message")
                 self.message_counter = 0
                 self.messages_required = random.randint(200, 300)
@@ -30,7 +30,7 @@ class MessageCounter(commands.Cog):
     @commands.command()
     @commands.has_role("Staff")
     async def countertest(self, ctx):
-        await ctx.channel.send(dictionary[random.randint(0, len(dictionary) - 1)].format(ctx.author.mention))
+        await ctx.channel.send(COUNTER_MESSAGES[random.randint(0, len(COUNTER_MESSAGES) - 1)].format(ctx.author.mention))
 
 
 def setup(bot):
