d399da3741bd8bfc834a8963686921e74a7c64c7
Felix422 <marius.mf2004@gmail.com>
2019-09-03 01:14:51 +0200

    removed old status and spacing

--- bot.py
+++ bot.py
@@ -8,10 +8,7 @@ logging.basicConfig(level=logging.ERROR)
 class Bot(commands.Bot):
 
     def __init__(self):
-        super().__init__(
-            command_prefix=COMMAND_PREFIX
-        )
-
+        super().__init__(command_prefix=COMMAND_PREFIX)
         self.remove_command("help")
 
     async def on_connect(self):
@@ -29,9 +26,6 @@ class Bot(commands.Bot):
             loop=self.loop,
             timeout=aiohttp.ClientTimeout(total=5)
         )
-
-        # await self.change_presence(status=discord.Status.online, activity=discord.Activity(name="all those bad suggestions", type=discord.ActivityType.watching))
-        # print("Changed presence")
         print(f"Bot Logged in as {self.user.name} and ready for duty!")
 
 Bot().run(BOT_TOKEN)
