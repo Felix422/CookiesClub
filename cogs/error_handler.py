abad58ecfaf019c0e916dd9ddc4639a914e8139e
Felix422 <marius.mf2004@gmail.com>
2019-10-11 00:33:13 +0200

    small optimisation

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -10,8 +10,10 @@ class EH(commands.Cog):
         error = getattr(error, 'original', error)
         if hasattr(ctx.command, 'on_error'):
             return
-        if isinstance(error, commands.errors.CommandNotFound):
-            return
+        if isinstance(error, discord.ext.commands.errors.CheckFailure):
+            pass
+        elif isinstance(error, commands.errors.CommandNotFound):
+            pass
         elif isinstance(error, commands.errors.CommandOnCooldown):
             await ctx.send(f"Command on cooldown, try again in {round(error.retry_after)}s")
         elif isinstance(error, commands.errors.MissingRole) or isinstance(error, commands.NotOwner):
@@ -20,18 +22,14 @@ class EH(commands.Cog):
             await ctx.send(f"{error.param.name} is a required argument that is missing")
         elif isinstance(error, commands.errors.BadArgument):
             await ctx.send(f"Bad argument passed")
-            return
         elif isinstance(error, commands.errors.ExtensionAlreadyLoaded):
             await ctx.send("Extension already loaded")
-            return
         elif isinstance(error, commands.errors.ExtensionNotFound):
             await ctx.send("Extension not found")
-            return
         elif isinstance(error, discord.Forbidden):
             await ctx.send("i don't have permission to do this!")
         elif isinstance(error, commands.errors.NotOwner):
             await ctx.send("You don't have permission for this")
-            return
         else:
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
             await ctx.send("Something broke <@285738922519035904>")
