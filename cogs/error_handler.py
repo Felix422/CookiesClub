4152ab75952762284e91b3bb9fafa6b4686c61c9
Felix422 <marius.mf2004@gmail.com>
2019-11-12 02:10:06 +0100

    fixed literally everything

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -8,28 +8,21 @@ class EH(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         error = getattr(error, 'original', error)
-        if hasattr(ctx.command, 'on_error'):
+        if any([hasattr(ctx.command, 'on_error'),
+                isinstance(error, commands.errors.CommandNotFound),
+                type(error) is commands.errors.CheckFailure]):
             return
-        if isinstance(error, discord.ext.commands.errors.CheckFailure):
-            pass
-        elif isinstance(error, commands.errors.CommandNotFound):
-            pass
         elif isinstance(error, commands.errors.CommandOnCooldown):
             await ctx.send(f"Command on cooldown, try again in {round(error.retry_after)}s")
-        elif isinstance(error, commands.errors.MissingRole) or isinstance(error, commands.NotOwner):
+        elif any([isinstance(error, commands.errors.MissingRole),
+                isinstance(error, commands.errors.NotOwner)]):
             await ctx.send("You don't have permission for this")
         elif isinstance(error, commands.errors.MissingRequiredArgument):
-            await ctx.send(f"{error.param.name} is a required argument that is missing")
+            await ctx.send(f"Missing argument {error.param.name}")
         elif isinstance(error, commands.errors.BadArgument):
             await ctx.send(f"Bad argument passed")
-        elif isinstance(error, commands.errors.ExtensionAlreadyLoaded):
-            await ctx.send("Extension already loaded")
-        elif isinstance(error, commands.errors.ExtensionNotFound):
-            await ctx.send("Extension not found")
         elif isinstance(error, discord.Forbidden):
             await ctx.send("i don't have permission to do this!")
-        elif isinstance(error, commands.errors.NotOwner):
-            await ctx.send("You don't have permission for this")
         else:
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
             await ctx.send("Something broke <@285738922519035904>")
