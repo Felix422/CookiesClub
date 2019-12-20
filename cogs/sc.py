0178faf062842d089a4cf5a936ccea24bd6427d9
Felix422 <marius.mf2004@gmail.com>
2019-12-20 23:05:49 +0100

    regex changes and better memory efficiency

--- cogs/sc.py
+++ cogs/sc.py
@@ -1,13 +1,15 @@
 import re
-import discord
+from discord import Member
+from discord.utils import get
 from asyncpg.exceptions import UniqueViolationError
-from discord.ext import commands
+from discord.ext.commands import Cog, command, check, group, has_role
 from utils.checks import is_channel_allowed
 
-class SC(commands.Cog):
+class SC(Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.regex = r'^(?P<name>.*?)(?P<tag>\(SC\s?#\s?[-?\d\+\s?]*\)|)$'
+        self.regex = re.compile(r'^(?P<name>.*?)(?P<tag>\(SC\s?#\s?[-?\d\+\s?]*\)|)$',
+                                re.IGNORECASE)
 
     @property
     def db(self):
@@ -15,7 +17,7 @@ class SC(commands.Cog):
 
     async def change_name(self, member, channel, showcase=0, mystery_showcase=0):
         '''function for the actual name change'''
-        stripped_name = re.search(self.regex, member.display_name, re.IGNORECASE).group('name')
+        stripped_name = self.regex.search(member.display_name).group('name')
         msc_tag = f'+{mystery_showcase}' if mystery_showcase != 0 else ''
         full_tag = f'(SC#{showcase}{msc_tag})'
         final_name = f'{stripped_name}{full_tag}'
@@ -25,8 +27,8 @@ class SC(commands.Cog):
         await member.edit(nick=final_name)
         await channel.send(f'Changed name to {final_name}')
 
-    @commands.group(invoke_without_command=True, aliases=['sc'])
-    @commands.check(is_channel_allowed)
+    @group(invoke_without_command=True, aliases=['sc'])
+    @check(is_channel_allowed)
     async def showcase(self, ctx, showcase='0', mystery_showcase='0'):
         '''adds a sc tag to a person'''
         try:
@@ -38,7 +40,7 @@ class SC(commands.Cog):
             await ctx.send('No')
             return
         if showcase < 0 or mystery_showcase < 0:
-            await ctx.send('SC count can't be negative!')
+            await ctx.send('SC count can\'t be negative!')
             return
         if showcase > 96 or mystery_showcase > 16:
             await ctx.send('SC count too high!')
@@ -47,19 +49,20 @@ class SC(commands.Cog):
             sc_trusted = await self.db.fetch('SELECT user_id FROM sc_trusted')
             trusted_list = [trusted_user['user_id'] for trusted_user in sc_trusted]
             allowed = any([ctx.author.id in trusted_list,
-                discord.utils.get(ctx.guild.roles, name='Staff') in ctx.author.roles,
+                get(ctx.guild.roles, name='Staff') in ctx.author.roles,
                 ctx.author.guild_permissions.manage_nicknames,
                 ctx.author.guild_permissions.change_nickname,
                 ctx.author.guild_permissions.administrator])
             if allowed:
                 await self.change_name(ctx.author, ctx.channel, showcase, mystery_showcase)
             else:
-                await ctx.send('''Showcases 70 and onward need staff permission or being on the trusted list to be used as a tag''')
+                await ctx.send(('Showcases 70 and onward need staff permission'
+                            ' or being on the trusted list to be used as a tag'))
         else:
             await self.change_name(ctx.author, ctx.channel, showcase, mystery_showcase)
 
     @showcase.command()
-    @commands.check(is_channel_allowed)
+    @check(is_channel_allowed)
     async def clear(self, ctx):
         '''removes the sc tag for a member'''
         if ctx.author.nick is not None:
@@ -67,8 +70,8 @@ class SC(commands.Cog):
             await ctx.author.edit(nick=oldnick)
 
     @showcase.command()
-    @commands.has_role('Staff')
-    async def trust(self, ctx, member: discord.Member):
+    @has_role('Staff')
+    async def trust(self, ctx, member: Member):
         '''Removes the sc70 limit for a person'''
         try:
             await self.db.fetch('INSERT INTO sc_trusted VALUES ($1)', member.id)
@@ -77,8 +80,8 @@ class SC(commands.Cog):
             await ctx.send('User already trusted')
 
     @showcase.command()
-    @commands.has_role('Staff')
-    async def untrust(self, ctx, member: discord.Member):
+    @has_role('Staff')
+    async def untrust(self, ctx, member: Member):
         '''Re-adds the sc70 limit for a person'''
         result = await self.db.execute('DELETE FROM sc_trusted WHERE user_id = $1', member.id)
         if result == 'DELETE 1':
@@ -88,7 +91,8 @@ class SC(commands.Cog):
 
     @showcase.command()
     async def update(self, ctx):
-        pass
+        name = self.regex.search(ctx.author.display_name).group('tag')
+
 
 def setup(bot):
     '''Setup function to add a cog'''
